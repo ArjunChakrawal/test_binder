1847d1473267732529e3472c695c934c6fb9a0c8
Arjun Chakrawal <chakrawalarjun9105@gmail.com>
2022-01-03 21:38:30 +0100

    c1

---
+++ test.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan  3 21:33:31 2022
+
+@author: Arjun
+"""
+
+import plotly.graph_objects as go
+import plotly.io as pio
+pio.renderers.default = "svg"
+fig = go.Figure(data =
+    go.Contour(
+        z=[[10, 10.625, 12.5, 15.625, 20],
+           [5.625, 6.25, 8.125, 11.25, 15.625],
+           [2.5, 3.125, 5., 8.125, 12.5],
+           [0.625, 1.25, 3.125, 6.25, 10.625],
+           [0, 0.625, 2.5, 5.625, 10]]
+    ))
+fig.show()
+
+import numpy as np
+import matplotlib.cm as cm
+import matplotlib.pyplot as plt
+
+
+delta = 0.025
+x = np.arange(-3.0, 3.0, delta)
+y = np.arange(-2.0, 2.0, delta)
+X, Y = np.meshgrid(x, y)
+Z1 = np.exp(-X**2 - Y**2)
+Z2 = np.exp(-(X - 1)**2 - (Y - 1)**2)
+Z = (Z1 - Z2) * 2
+
+fig, ax = plt.subplots()
+CS = ax.contour(X, Y, Z)
+ax.clabel(CS, inline=True, fontsize=10)
+ax.set_title('Simplest default with labels')
